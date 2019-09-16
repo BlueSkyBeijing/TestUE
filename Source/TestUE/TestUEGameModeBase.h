d81652d9dd3ed1d20837d56373e3057779e4854c
chenjunlong@kingsoft.com <chenjunlong@kingsoft.com>
2019-09-16 12:06:35 +0800

    Add test project base frame files

---
+++ Source/TestUE/TestUEGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestUEGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTUE_API ATestUEGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
