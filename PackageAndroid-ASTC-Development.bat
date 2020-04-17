3817d532b45e80b669fbf2bea439b46026146b0a
chenjunlong@kingsoft.com <chenjunlong@kingsoft.com>
2020-04-17 11:09:02 +0800

    update pak bat

--- PackageAndroid-ASTC-Development.bat
+++ PackageAndroid-ASTC-Development.bat
@@ -3,7 +3,7 @@ set PROJECT_FILE=G:\TestUE\TestUE.uproject
 set ARCHIVED_DIR=G:\TestUE\Build
 
 
-%UE4_ROOT%Engine/Build/BatchFiles/RunUAT.bat BuildCookRun -project=%PROJECT_FILE% -clientconfig=Development -archivedirectory=%ARCHIVED_DIR% -cookflavor=ASTC -targetplatform=Android -skipserver -client -nop4 -nocompileeditor -build -cook -cookall -stage -archive -package -pak -prereqs -compressed -unattended -utf8output -nodebuginfo
+%UE4_ROOT%Engine/Build/BatchFiles/RunUAT.bat BuildCookRun -project=%PROJECT_FILE% -clientconfig=Development -archivedirectory=%ARCHIVED_DIR% -cookflavor=ASTC -targetplatform=Android -skipserver -nop4 -nocompileeditor -build -cook -cookall -stage -archive -package -pak -prereqs -compressed -unattended -utf8output -nodebuginfo
 
 
 pause
